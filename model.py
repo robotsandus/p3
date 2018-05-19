7c61fa3b8d9ab80e86d14090b797bbab24afe0bd
obelix <theholeinmysole@gmail.com>
2018-05-19 21:54:51 +0200

    update model.py

--- model.py
+++ model.py
@@ -17,7 +17,7 @@ with open('udacity_data/data/driving_log.csv') as csvfile:
 
 
 from sklearn.model_selection import train_test_split
-train_samples, validation_samples = train_test_split(lines, test_size=0.2)
+train_samples, validation_samples = train_test_split(lines, test_size=0.4)
 
 import cv2
 import numpy as np
@@ -45,21 +45,131 @@ def generator(samples, batch_size=32):
                 angles.append(center_angle)
 
             # trim image to only see section with road
-            X_train = np.array(images)
-            y_train = np.array(angles)
+            X_train_x = np.array(images)
+            y_train_y = np.array(angles)
             yield sklearn.utils.shuffle(X_train, y_train)
+            #return (X_train_x, y_train_y)
 
 # compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=128)
-validation_generator = generator(validation_samples, batch_size=128)
+train_generator = generator(train_samples, batch_size=64)
+validation_generator = generator(validation_samples, batch_size=64)
+#visual_data_generator = generator(validation_samples, batch_size=32)
+
+
+
+def displayCV2(img):
+    '''
+    Utility method to display a CV2 Image
+    '''
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def process_img_for_visualization(image, angle, pred_angle, frame):
+    '''
+    Used by visualize_dataset method to format image prior to displaying. Converts colorspace back to original BGR, applies text to display steering angle and frame number (within batch to be visualized), and applies lines representing steering angle and model-predicted steering angle (if available) to image.
+    '''    
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    img = cv2.cvtColor(image, cv2.COLOR_YUV2BGR)
+    img = cv2.resize(img,None,fx=3, fy=3, interpolation = cv2.INTER_CUBIC)
+    h,w = img.shape[0:2]
+    # apply text for frame number and steering angle
+    cv2.putText(img, 'frame: ' + str(frame), org=(2,18), fontFace=font, fontScale=.5, color=(200,100,100), thickness=1)
+    cv2.putText(img, 'angle: ' + str(angle), org=(2,33), fontFace=font, fontScale=.5, color=(200,100,100), thickness=1)
+    # apply a line representing the steering angle
+    cv2.line(img,(int(w/2),int(h)),(int(w/2+angle*w/4),int(h/2)),(0,255,0),thickness=4)
+    if pred_angle is not None:
+        cv2.line(img,(int(w/2),int(h)),(int(w/2+pred_angle*w/4),int(h/2)),(0,0,255),thickness=4)
+    return img
+def visualize_dataset(X,y,y_pred=None):
+    '''
+    format the data from the dataset (image, steering angle) and display
+    '''
+    for i in range(len(X)):
+        if y_pred is not None:
+            img = process_img_for_visualization(X[i], y[i], y_pred[i], i)
+        else: 
+            img = process_img_for_visualization(X[i], y[i], None, i)
+        displayCV2(img)        
+
+def preprocess_image(img):
+    '''
+    Method for preprocessing images: this method is the same used in drive.py, except this version uses
+    BGR to YUV and drive.py uses RGB to YUV (due to using cv2 to read the image here, where drive.py images are 
+    received in RGB)
+    '''
+    # original shape: 160x320x3, input shape for neural net: 66x200x3
+    # crop to 105x320x3
+    #new_img = img[35:140,:,:]
+    # crop to 40x320x3
+    new_img = img[50:140,:,:]
+    # apply subtle blur
+    new_img = cv2.GaussianBlur(new_img, (3,3), 0)
+    # scale to 66x200x3 (same as nVidia)
+    new_img = cv2.resize(new_img,(200, 66), interpolation = cv2.INTER_AREA)
+    # scale to ?x?x3
+    #new_img = cv2.resize(new_img,(80, 10), interpolation = cv2.INTER_AREA)
+    # convert to YUV color space (as nVidia paper suggests)
+    new_img = cv2.cvtColor(new_img, cv2.COLOR_BGR2YUV)
+    return new_img
+def generate_training_data_for_visualization(image_paths, angles, batch_size=10, validation_flag=False):
+    '''
+    method for loading, processing, and distorting images
+    if 'validation_flag' is true the image is not distorted
+    '''
+    X = []
+    y = []
+    image_paths, angles = shuffle(image_paths, angles)
+    for i in range(batch_size):
+        print(image_paths[i])
+        img = cv2.imread(image_paths[i])
+        angle = angles[i]
+        img = preprocess_image(img)
+        if not validation_flag:
+            img, angle = random_distort(img, angle)
+        X.append(img)
+        y.append(angle)
+    return (np.array(X), np.array(y))
+
+def random_distort(img, angle):
+    ''' 
+    method for adding random distortion to dataset images, including random brightness adjust, and a random
+    vertical shift of the horizon position
+    '''
+    new_img = img.astype(float)
+    # random brightness - the mask bit keeps values from going beyond (0,255)
+    value = np.random.randint(-28, 28)
+    if value > 0:
+        mask = (new_img[:,:,0] + value) > 255 
+    if value <= 0:
+        mask = (new_img[:,:,0] + value) < 0
+    new_img[:,:,0] += np.where(mask, 0, value)
+    # random shadow - full height, random left/right side, random darkening
+    h,w = new_img.shape[0:2]
+    mid = np.random.randint(0,w)
+    factor = np.random.uniform(0.6,0.8)
+    if np.random.rand() > .5:
+        new_img[:,0:mid,0] *= factor
+    else:
+        new_img[:,mid:w,0] *= factor
+    # randomly shift horizon
+    h,w,_ = new_img.shape
+    horizon = 2*h/5
+    v_shift = np.random.randint(-h/8,h/8)
+    pts1 = np.float32([[0,horizon],[w,horizon],[0,h],[w,h]])
+    pts2 = np.float32([[0,horizon+v_shift],[w,horizon+v_shift],[0,h],[w,h]])
+    M = cv2.getPerspectiveTransform(pts1,pts2)
+    new_img = cv2.warpPerspective(new_img,M,(w,h), borderMode=cv2.BORDER_REPLICATE)
+    return (new_img.astype(np.uint8), angle)
 
 ch, row, col = 3, 160, 320  # Trimmed image format
 
 
 
-batch_size = 128
+batch_size = 64
 
 images = []
+paths = []
 steering_angle_measurements = []
 
 counter = 0
@@ -71,13 +181,16 @@ for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
     current_path = 'udacity_data/data/IMG/' + filename
+
     image = cv2.imread(current_path)
+    paths.append(current_path)
     images.append(image)
     measurement = float(line[3])
     steering_angle_measurements.append(measurement)
 
 X_train = np.array(images)
 y_train = np.array(steering_angle_measurements)
+
 
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout, ELU, Activation, ZeroPadding2D
@@ -127,14 +240,15 @@ model.add(Dense(1,name='output'))
 
 model.compile(optimizer=Adam(lr= 0.0001), loss="mse")
 
+#visualize_dataset(visual_data_generator_X,visual_data_generator_y)
 
-history_object = model.fit(X_train,y_train, validation_split=0.3,shuffle=True,nb_epoch=5)
-#history_object = model.fit_generator(train_generator, samples_per_epoch = len(train_samples),validation_data=validation_generator, nb_val_samples=len(validation_samples), nb_epoch=10)
+#history_object = model.fit(X_train,y_train, validation_split=0.3,shuffle=True,nb_epoch=5)
+#history_object = model.fit_generator(train_generator, steps_per_epoch=10, validation_steps=3215, epochs=5, validation_data=validation_generator)
 
 #history_object = model.fit_generator()
 
 
-#history_object = model.fit_generator(train_generator, steps_per_epoch= len(train_samples),validation_data=validation_generator, validation_steps=len(validation_samples), epochs=5, verbose = 1)
+history_object = model.fit_generator(train_generator, steps_per_epoch= len(train_samples),validation_data=validation_generator, validation_steps=len(validation_samples), epochs=5, verbose = 1)
 
 ### print the keys contained in the history object
 print(history_object.history.keys())
@@ -147,5 +261,10 @@ plt.ylabel('mean squared error loss')
 plt.xlabel('epoch')
 plt.legend(['training set', 'validation set'], loc='upper right')
 plt.show()
+
+print(model.summary())
+
+X_axis, y_axis = generate_training_data_for_visualization(paths, steering_angle_measurements)
+visualize_dataset(X_axis, y_axis)
 
 model.save('model_udacity_1.h5')
